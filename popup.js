600b1c6db9037165ab1ad7b3226748020b499b1e
cyamax <widerange1216@gmail.com>
2024-10-20 03:07:45 +0900

    Add CSS

--- popup.js
+++ popup.js
@@ -8,50 +8,46 @@ document.getElementById('getFilesButton_slides').addEventListener('click', funct
 function getRecentFiles(fileType) {
   chrome.runtime.sendMessage({ action: "getRecentFiles", fileType: fileType }, function(response) {
     // 初期化
-    document.getElementById("fileList").innerHTML = "<li></li>";
+    document.getElementById("fileList").innerHTML = "";
     if (response.error) {
-      document.getElementById("fileList").innerHTML = "<li>" + response.error + "</li>";
+      document.getElementById("fileList").innerHTML = response.error;
       return;
     }
 
     const fileList = document.getElementById("fileList");
     response.files.forEach(file => {
-      const li = document.createElement("li");
-      const a = document.createElement("a");
+
       const timeStamp = new Date(file.lastVisitTime);
-      a.href = file.webViewLink;
-      a.textContent = "[" + file.visitCount + "] " +timeStamp.toLocaleDateString() + " " +removeTrailingStrings(file.name);
-      a.target = "_blank";
+      const textContent = removeTrailingStrings(file.name);
 
       // アイコンを追加する
-      const icon = document.createElement("img");
+      let icon = ""
         if (/document/.test(file.webViewLink)) {
-          icon.src = "images/docs_192px.svg";
+          icon = "images/docs_192px.svg";
         } else if (/spreadsheets/.test(file.webViewLink)) {
-          icon.src = "images/sheets_192px.svg"; 
+          icon = "images/sheets_192px.svg"; 
         } else if (/presentation/.test(file.webViewLink)) {
-          icon.src = "images/slides_192px.svg"; 
+          icon = "images/slides_192px.svg"; 
         } else if (/forms/.test(file.webViewLink)) {
-          icon.src = "images/forms_192px.svg";
+          icon = "images/forms_192px.svg";
         } else if (/drive/.test(file.webViewLink)) {
-          icon.src = "images/folder_192px.svg";
+          icon = "images/folder_192px.svg";
         } else {
-          icon.src = "images/folder_192px.svg"; // それ以外のファイルのアイコンのパス
+          icon = "images/folder_192px.svg"; // それ以外のファイルのアイコンのパス
         }
 
-      // アイコンのサイズを設定
-      icon.width = 16;  // 例：幅を16pxに設定
-      icon.height = 16; // 例：高さを16pxに設定
-
-      icon.alt = file.mimeType; // アイコンの説明
-      li.appendChild(icon); // アイコンをli要素に追加
-
-
-      li.appendChild(a);
-      fileList.appendChild(li);
+        const historyItemHTML = `<a href="${file.webViewLink}" target="_blank"><div class="history-item">
+            <img class="file-icon doc" src="${icon}" width="32" height="32">
+          <div class="file-info">
+            <p class="file-name">${textContent}</p>
+            <span class="access-time">${timeStamp.toLocaleDateString()}</span>
+          </div>
+        </div></a>`;
+      fileList.insertAdjacentHTML('beforeend', historyItemHTML);
     });
   });
 }
+
 
 function removeTrailingStrings(str) {
   const patterns = [
@@ -72,6 +68,21 @@ function removeTrailingStrings(str) {
   return str; // パターンに一致しない場合は元の文字列を返す
 }
 
+
+// タブ切り替えの実装
+document.querySelectorAll('.tab').forEach(tab => {
+    tab.addEventListener('click', () => {
+        // アクティブなタブのスタイルを削除
+        document.querySelector('.tab.active').classList.remove('active');
+        // クリックされたタブをアクティブに
+        tab.classList.add('active');
+        
+        // ここにフィルタリングのロジックを追加
+        const fileType = tab.textContent.trim();
+        // console.log('Selected file type:', fileType);
+        // 実際のフィルタリング処理をここに実装
+    });
+    });
 
 // 初回起動時
 getRecentFiles(0)
