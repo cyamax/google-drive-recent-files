b5308f72be202714cb9d3e751711b7efe5eff2ac
TakanoriYamashita <t-yamashita@mercari.com>
2024-10-19 22:30:40 +0900

    Add Icons & Search Function

--- background.js
+++ background.js
@@ -1,27 +1,32 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "getRecentFiles") {
-      // Google Docs、スライド、スプレッドシートの URL を含む正規表現でフィルタリング
-      const urlPattern = /https:\/\/docs.google.com\//; 
+      const urlPattern = /https:\/\/(docs|drive).google.com\//; 
+      const searchWords = ['https://drive.google.com', 'https://docs.google.com','https://docs.google.com/document']
+      const word = searchWords[1]
       chrome.history.search({
-        'text': '', // 空文字列を指定してすべての履歴を取得
+        'text': word,
         'startTime': 0, 
-        'maxResults': 20 
+        'maxResults': 100 
       }, function(historyItems) {
         const files = [];
         let lastUrl = ""; // 直前に追加した URL を保存する変数
-  
+        console.table(historyItems)
         historyItems.forEach(item => {
-            if (urlPattern.test(item.url)) {
+            // if (urlPattern.test(item.url)) {
+            if (item.url.includes(word)) {
               // URL から引数を削除
               const urlWithoutParams = new URL(item.url).origin + new URL(item.url).pathname;
-      
               if (urlWithoutParams !== lastUrl) {
                 files.push({
                   name: item.title,
-                  webViewLink: item.url
+                  webViewLink: item.url,
+                  lastVisitTime: item.lastVisitTime,
+                  visitCount: item.visitCount
                 });
                 lastUrl = urlWithoutParams;
               }
+            } else {
+              // console.table(item)
             }
         });
   
